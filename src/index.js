0c8ad6f03f560454b86d2d761553720326bab48b
Maks <maks135254@gmail.com>
2024-03-05 16:18:34 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from 'components/App';
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/medicine-delivery-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
